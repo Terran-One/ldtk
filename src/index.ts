9210ad3d28af4dd3e1a4072bdb670ab54af4d39a
kiruse <26940742+Kiruse@users.noreply.github.com>
2023-02-26 19:09:08 +0100

    add dump util for inspecting ASTs

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as generate } from './codegen';
 export { Lexer, Parser } from './langkit';
+export * as utils from './utils';
---
+++ src/utils.ts
@@ -0,0 +1,58 @@
+import { Token } from 'antlr4ts/Token';
+import chalk from 'chalk';
+import fs, { FileHandle } from 'fs/promises';
+
+export type AntlrCtx = {
+  _start: Token;
+  _stop: Token | undefined;
+}
+type RuleAST = {
+  type: string;
+  children: ASTLike[] | never[];
+  ctx: AntlrCtx;
+}
+type LabelAST = {
+  type: string;
+  family: 'labels';
+  choice: RuleAST;
+  children: [RuleAST];
+  ctx: AntlrCtx;
+}
+type ASTLike = RuleAST | LabelAST;
+
+export async function dump(src: string, ast: ASTLike, file: string | FileHandle = '', level = 0) {
+  if (file && typeof file === 'string')
+    file = await fs.open(file, 'w');
+  return await dump_inner(src, ast, level);
+}
+
+async function dump_inner(src: string, ast: ASTLike, level: number) {
+  const indent = '  '.repeat(level);
+  const isLabel = 'family' in ast && ast.family === 'labels';
+  
+  let line = '';
+  
+  if (isLabel) {
+    line += `${indent}${chalk.green(ast.choice.type)}`;
+  } else {
+    line += `${indent}${chalk.cyan(ast.type)}`;
+  }
+  
+  if (ast.ctx._stop) {
+    line += ': ' + chalk.yellow(getCodeRange(src, ast.ctx._start, ast.ctx._stop).replace(/\r?\n/g, '\\n'));
+  }
+  
+  console.log(line);
+    
+  if (isLabel) {
+    ast.choice.children.forEach(child => dump_inner(src, child, level + 1));
+  } else {
+    ast.children.forEach(child => dump_inner(src, child, level + 1));
+  }
+}
+
+export function getCodeRange(src: string, start: Token, stop: Token) {
+  const startIdx = start.startIndex;
+  const stopIdx = stop.stopIndex;
+  return src.substring(startIdx, stopIdx + 1);
+}
